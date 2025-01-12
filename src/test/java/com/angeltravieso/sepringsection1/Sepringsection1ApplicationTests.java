7cfa7fd364f53c2578e71a7c212e450e066b9e54
AngelTravieso <angeltraviesoc@gmail.com>
2025-01-12 19:30:15 -0400

    Initial Commit

---
+++ src/test/java/com/angeltravieso/sepringsection1/Sepringsection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angeltravieso.sepringsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sepringsection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
