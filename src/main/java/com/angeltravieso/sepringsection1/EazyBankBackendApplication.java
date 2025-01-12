8ebe50ebbc2299cf0fc4b4cacb955cb747d0b535
AngelTravieso <angeltraviesoc@gmail.com>
2025-01-12 19:46:11 -0400

    Creating a simple Spring Boot app with out security

--- src/main/java/com/angeltravieso/sepringsection1/EazyBankBackendApplication.java
+++ src/main/java/com/angeltravieso/sepringsection1/EazyBankBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sepringsection1Application {
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sepringsection1Application.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
--- src/test/java/com/angeltravieso/sepringsection1/Sepringsection1ApplicationTests.java
+++ src/test/java/com/angeltravieso/sepringsection1/Sepringsection1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sepringsection1ApplicationTests {
+class EazyBankBackendApplicationTest {
 
 	@Test
 	void contextLoads() {
